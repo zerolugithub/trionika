af9ee48fe1c46e1177f1ab7e4ae57ed1b0dfe504
Trionika <vadimshurhal@gmail.com>
2018-01-12 13:45:44 +0200

    Change path to chromedriver on jenkins directory

--- tools.py
+++ tools.py
@@ -42,7 +42,7 @@ class config_browser():
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument("--window-size=1920,1080")
         #driver = webdriver.Chrome(executable_path='/Users/user/Desktop/chromedriver', chrome_options=chrome_options)
-        driver = webdriver.Chrome(executable_path='/root/.wdm/chromedriver/2.35/chromedriver', chrome_options=chrome_options)    #### <----- Path to chomedriver on server
+        driver = webdriver.Chrome(executable_path='/var/lib/jenkins/.wdm/chromedriver/2.34/chromedriver', chrome_options=chrome_options)    #### <----- Path to chomedriver on server
         browser.set_driver(driver)
 
 
